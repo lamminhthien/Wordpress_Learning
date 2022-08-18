c3f3bf12ca8c5f6ef800213d23b3d758b56bcde4
Thiện Lâm <lamminhthien02012000@gmail.com>
2022-08-18 19:12:21 +0700

    init template

---
+++ app/public/wp-content/themes/abc-theme/template-parts/content.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The default template for displaying content
+ *
+ * Used for both singular and index.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package ABC_WP
+ * @subpackage CDE_Theme
+ * @since CDE Theme 1.0
+ */
+?>
